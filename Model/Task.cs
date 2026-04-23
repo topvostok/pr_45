045162426f3b4758df96e7ff88a32888cb406a9b
Linashihova <linashikhova@mail.ru.>
2026-04-23 09:51:57 +0500

    Разработанные модели данных. Пешин

---
+++ Model/Task.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ASP_GET.Model
+{
+    /// <summary>
+    /// Задачи
+    /// </summary>
+    public class Tasks
+    {
+        /// <summary>
+        /// Код
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Приоритет
+        /// </summary>
+        public string Priority { get; set; }
+
+        /// <summary>
+        /// Дата выполнения
+        /// </summary>
+        public DateTime DateTimeExecute { get; set; }
+
+        /// <summary>
+        /// Комментарий
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// Выполнено
+        /// </summary>
+        public bool Done { get; set; }
+    }
+}
