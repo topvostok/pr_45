9e071c8f58bc66ba47757280264678489ab3be0e
Linashihova <linashikhova@mail.ru.>
2026-04-23 09:55:57 +0500

    Разработанный контекст для загрузки данных Peshin

---
+++ Context/TaskContext.cs
@@ -0,0 +1,27 @@
+﻿using ASP_GET.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ASP_GET.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated(); 
+            Tasks.Load(); 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+           
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;" +
+                "uid=root;" +
+                "pwd=;" +
+                "database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+    }
+}
